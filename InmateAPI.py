7c38beb1b094822ec01c91ca09ebfe6b962369d6
Daniel <Danielnoren2@gmail.com>
2021-11-18 17:33:26 +0100

    Added combined API

--- InmateAPI.py
+++ InmateAPI.py
@@ -3,15 +3,14 @@ import requests
 import json
 #from PIL import Image
 
-save = False
+save = True
 url = "http://www.JailBase.com/api/1/search/"
-params = {'source_id' : 'az-mcso', 'last_name' : 'jensen'}
+params = {'source_id' : 'ca-sfso', 'last_name' : 'Smith'}
 response = requests.get(url , params = params)
 
 if response.status_code == 200: #The API returns the Ok() http status code when everything is okay
     if len(response.json()["records"]) > 0:
         records = response.json()["records"]
-        print(type(records))
         sortedRecords = sorted(records, key=lambda k: k['book_date'], reverse=True)
         print(sortedRecords)
     else : 
@@ -19,23 +18,27 @@ if response.status_code == 200: #The API returns the Ok() http status code when 
 else:
     print("Http status code : ", response.status_code)
 
+date = str(response.json()["records"][0]["book_date"])
+date = date.replace("-", "/")
+print(date)
+#%%%
+
+url = "https://www.metaweather.com/api/location/2487956/" + date
+print(url)
+response = requests.get(url)
+weather = response.json()
+print(weather)
+
+
+#%%%
+
 #Saving as json file
 if save :
     with open('records.json', 'w') as outfile:
         json.dump(records, outfile)
+    with open('weather.json', 'w') as outfile:
+        json.dump(weather, outfile)
 else :
     exit()
 
 
-
-
-
-
-
-#%%%
-# Reading image
-#from PIL import Image # pip3 install Pillow
-
-#url = 'https://imgstore.jailbase.com/small/arrested/az-mcso/2020-10-28/trevon-tajee-smith-t662412.pic1.jpg'
-#im = Image.open(requests.get(url, stream=True).raw)
-#im
